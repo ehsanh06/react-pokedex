638486a98339660cbe13beba26254ad987536e77
gameyfi <ehsanhussain991@gmail.com>
2018-07-09 12:52:54 +0100

    created PokeCell component

---
+++ src/components/PokeCell.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/PokeList.css';
+
+const PokeCell = () => {
+    return <button class="poke-list__poke-cell"></button>
+};
+
+export default PokeCell;
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PokeCell from './PokeCell';
+
 import './styles/PokeList.css';
+
 
 const PokeList = () => {
     return (
         <section className="poke-list__section">
-        
+            <PokeCell />        
+            <PokeCell />        
+            <PokeCell />        
         </section>
     )
 }
