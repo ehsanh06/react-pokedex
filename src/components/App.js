70ade286caf47df9699cb58b1abbf8f564b1000e
gameyfi <ehsanhussain991@gmail.com>
2018-07-09 12:34:23 +0100

    basic app skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import './styles/App.css';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
-        HELLO WORLD
+
       </div>
     );
   }
