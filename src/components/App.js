edf6c8ad4abd4fd8f50927f46e9e5d95f74ca584
gameyfi <ehsanhussain991@gmail.com>
2018-07-09 14:25:20 +0100

    Fetching data and state management

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import DetailView from './DetailView';
 import PokeList from './PokeList';
+import DetailView from './DetailView';
+import Pokemon from '../Pokemon';
 
 import './styles/App.css';
 
@@ -8,20 +9,33 @@ export default class App extends Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      pokemon: {}
+    };
 
     this.handleOnClick = this.handleOnClick.bind(this);
   }
 
   handleOnClick = (id) => {
-    console.log(id);
+    // Debugging
+    // console.log(id);
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(response => response.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+
+        // Debugging Data
+        console.log(pokemon);
+        this.setState({ pokemon });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
     return (
       <div className="App">
         <PokeList handleOnClick={this.handleOnClick}/>
-        <DetailView />
+        <DetailView pokemon={this.state.pokemon} />
       </div>
     );
   }
