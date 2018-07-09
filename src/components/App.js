6eff53502f4a709cb17cfbefa837aa0f8a830d9c
gameyfi <ehsanhussain991@gmail.com>
2018-07-09 12:43:42 +0100

    PokeList component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
+import PokeList from './PokeList';
+
 export default class App extends Component {
 
   constructor() {
@@ -11,7 +13,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <PokeList />
       </div>
     );
   }
